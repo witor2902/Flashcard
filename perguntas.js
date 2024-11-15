fcf46dc798bc37f32bcd8185906d5a6d338f8ee3
witor2902 <00001131939724sp@al.educacao.sp.gov.br>
2024-11-14 23:37:10 -0300

    Add files via upload
    Projeto terminado.

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Título nacional',
+    'Quado foi o primeiro título nacional conquistado pelo Flamengo?',
+    'Campeonato Brasileiro em 1980'
+)
+
+criaCartao(
+    'Campeonato Brasileiro',
+    'Quatas vezes o Flamengo já conquistou o Campeonato Brasileiro?',
+    'OIto vezes(1980, 1982, 1983, 1992, 2009, 2019, 2020, 2021).'
+)
+
+criaCartao(
+    'Livros',
+    'Qual é o livro mais vendido de todos os tempos?',
+    'Flamengo 15x1 contra o Mangueira, em 1920'
+)
+
+criaCartao(
+    'Maracanã',
+    'Qual o recore de público em um jogo do Flamengo no Maracanã?',
+    'Cerca de 194 mil pessoas, em 1963, contra o Fluminese'
+)
